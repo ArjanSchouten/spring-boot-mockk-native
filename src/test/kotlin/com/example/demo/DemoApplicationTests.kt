792bbea0d5fc4470b81abac6aa1f7961661b2462
ArjanSchouten <arjan.schouten@solureal.com>
2023-01-13 18:02:39 +0100

    add reproducable example of failing test with MockkExtension.
    run ./gradlew nativeTest

---
+++ src/test/kotlin/com/example/demo/DemoApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.example.demo
+
+import io.mockk.junit5.MockKExtension
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.boot.test.context.SpringBootTest
+
+@ExtendWith(MockKExtension::class)
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
